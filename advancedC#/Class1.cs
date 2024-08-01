f8794e0e6579446b33e4a0886b5d1d4722aa951d
mirna <mirna@DESKTOP-NVAPE0C>
2024-08-01 17:42:32 +0300

    Add project files.

---
+++ advancedC#/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advancedC_
+{
+   
+
+        public class Range<T> where T : IComparable<T>
+        {
+          
+            public T Min { get; private set; }
+            public T Max { get; private set; }
+
+
+            public Range(T min, T max)
+            {
+                if (min.CompareTo(max) > 0)
+                {
+                    throw new ArgumentException("Minimum value cannot be greater than maximum value.");
+                }
+
+                Min = min;
+                Max = max;
+            }
+
+           
+            public bool IsInRange(T value)
+            {
+                return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+            }
+
+        
+            public T Length()
+            {
+                dynamic min = Min;
+                dynamic max = Max;
+                return max - min;
+            }
+        }
+    }
+
+
---
+++ advancedC#/Program.cs
@@ -0,0 +1,77 @@
+﻿using advancedC_;
+using System;
+
+class Program
+{
+    static void PrintArray<T>(T[] array)
+    {
+        foreach (T item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+    static void BubbleSortOptimized<T>(T[] array) where T : IComparable<T>
+    {
+        int n = array.Length;
+        bool swapped;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j].CompareTo(array[j + 1]) > 0)
+                {
+
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+                break;
+        }
+    }
+    static void Main()
+    {
+
+        #region q1
+        int[] intArray = { 64, 34, 25, 12, 22, 11, 90 };
+        Console.WriteLine("Original integer array:");
+        PrintArray(intArray);
+
+        BubbleSortOptimized(intArray);
+
+        Console.WriteLine("Sorted integer array:");
+        PrintArray(intArray);
+
+        string[] stringArray = { "banana", "apple", "cherry", "date" };
+        Console.WriteLine("Original string array:");
+        PrintArray(stringArray);
+
+        BubbleSortOptimized(stringArray);
+
+        Console.WriteLine("Sorted string array:");
+        PrintArray(stringArray);
+        #endregion
+
+        #region q2
+        var intRange = new Range<int>(1, 10);
+        Console.WriteLine($"Range: {intRange.Min} to {intRange.Max}");
+        Console.WriteLine($"Is 5 in range? {intRange.IsInRange(5)}");
+        Console.WriteLine($"Length of range: {intRange.Length()}");
+
+        var doubleRange = new Range<double>(1.5, 5.5);
+        Console.WriteLine($"\nRange: {doubleRange.Min} to {doubleRange.Max}");
+        Console.WriteLine($"Is 3.5 in range? {doubleRange.IsInRange(3.5)}");
+        Console.WriteLine($"Length of range: {doubleRange.Length()}"); 
+        #endregion
+    }
+
+  
+}
